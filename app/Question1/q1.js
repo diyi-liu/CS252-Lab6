7c649abed1fd930a7be9b7c3d7eb01fb6a123411
Diyi Liu <diyi.liu@outlook.com>
2018-12-05 21:21:33 -0500

    Structure Initialized

---
+++ app/Question1/q1.js
@@ -0,0 +1,8 @@
+'use strict';
+
+
+$("#message-box").on('input', function() {
+    var scroll_height = $("#message-box").get(0).scrollHeight;
+
+    $("#message-box").css('height', scroll_height + 'px');
+});
